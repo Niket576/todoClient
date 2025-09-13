8ef12dfbe6320834fbc245a5dc97ed2c91c9716a
Niket576 <niketchaudhary576@gmail.com>
2025-09-13 21:08:08 +0530

    changes in authservices.js

--- src/Services/AuthServices.js
+++ src/Services/AuthServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/v1", // or just http://localhost:8080
+    baseURL: process.env.REACT_APP_API_URL, // or just http://localhost:8080
 });
 
 // Services
