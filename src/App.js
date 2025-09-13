1ab4424670a8fceafe854be1e1eddbbf60dae0b6
Niket576 <niketchaudhary576@gmail.com>
2025-09-13 23:08:34 +0530

    removed the hashrouter

--- src/App.js
+++ src/App.js
@@ -1,61 +1,33 @@
-// import { Routes, Route } from 'react-router-dom';
-// import Landing from './pages/Landing/Landing.js';
-// import Homepage from './pages/Home/Homepage.js'
-// import Register from './pages/Auth/Register.js';
-// import Login from './pages/Auth/Login.js';
-// import About from './pages/About/About.js';
-// import TodoList from './pages/Todos/TodoList.js';
-// import { Toaster } from "react-hot-toast";
-
-
-// function App() {
-//   return (
-//     <div >
-//       <Routes>
-//         <Route path='/' element={<Landing />} />
-//         <Route path='/home' element={<Homepage />} />
-//         <Route path='/register' element={<Register />} />
-//         <Route path='/login' element={<Login />} />
-//         <Route path='/about' element={<About />} />
-//         <Route path='/todoList' element={<TodoList />} />
-//       </Routes>
-
-//       <Toaster />
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing/Landing.js';
-import Homepage from './pages/Home/Homepage.js';
+import Homepage from './pages/Home/Homepage.js'
 import Register from './pages/Auth/Register.js';
 import Login from './pages/Auth/Login.js';
 import About from './pages/About/About.js';
 import TodoList from './pages/Todos/TodoList.js';
 import { Toaster } from "react-hot-toast";
 
+
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/home' element={<Homepage />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/todoList' element={<TodoList />} />
-        </Routes>
+    <div >
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<Homepage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/todoList' element={<TodoList />} />
+      </Routes>
 
-        <Toaster />
-      </div>
-    </HashRouter>
+      <Toaster />
+
+    </div>
   );
 }
 
 export default App;
+
+
+
+
